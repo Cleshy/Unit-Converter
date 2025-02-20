a2192f85bafa33a8db16c562e8275e42083e8a2f
Czipa Szilárd <szilard.czipa@gmail.com>
2025-02-20 13:53:27 +0100

    Finished HTML structure - added units

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ Requirements:
 - Round the numbers down to three decimal places
 
 */
+
+const METER_TO_FEET = 3.281;
+const LITER_TO_GALLON = 0.264;
+const KILOGRAM_TO_POUND = 2.204;
+
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+
+const inputTexts = document.querySelectorAll(".input-value");
+const conversionTexts = document.querySelectorAll(".conversion");
+
+conversionTexts.forEach((conversion) => {});
