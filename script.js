90edff831f67bcfe65081f10be677508f4592205
Czipa Szilárd <szilard.czipa@gmail.com>
2025-03-24 13:06:05 +0100

    Refactoring - semantic html, cleaner JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/*
-
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-
-Requirements:
-
-- Follow the design spec
-- Generate all conversions when the user clicks "Convert"
-- Round the numbers down to three decimal places
-
-*/
-
 const CONVERSION_RATES = {
   meterToFeet: 3.281,
   feetToMeter: 0.3048,
@@ -21,29 +7,32 @@ const CONVERSION_RATES = {
   poundToKg: 0.4536,
 };
 
+const INPUT_MIN_VALUE = 1;
+const INPUT_MAX_VALUE = 10000;
+
 const inputEl = document.getElementById("input-el");
 const convertBtn = document.getElementById("convert-btn");
 const iconContainer = document.querySelector(".icon-container");
 const errorMessage = document.querySelector(".error-message");
 
-console.log(iconContainer);
-
 const inputTexts = document.querySelectorAll(".input-value");
 const conversionTexts = document.querySelectorAll(".conversion");
 
 convertBtn.addEventListener("click", function () {
-  const inputValue = parseFloat(inputEl.value);
+  if (
+    inputEl.value >= INPUT_MIN_VALUE &&
+    inputEl.value <= INPUT_MAX_VALUE &&
+    !isNaN(inputEl.value)
+  ) {
+    const inputValue = parseFloat(inputEl.value);
 
-  if (!isNaN(inputValue) && inputValue > 0) {
     const conversions = calculateConversions(inputValue);
     setCurrentValueTexts(inputValue);
     setConversionValues(conversions);
   } else {
-    errorMessage.classList.add("show");
-
-    setTimeout(function () {
-      errorMessage.classList.remove("show");
-    }, 2000);
+    showErrorMessage(
+      `Please enter a number between ${INPUT_MIN_VALUE} and ${INPUT_MAX_VALUE}!`
+    );
   }
 });
 
@@ -86,19 +75,40 @@ function setConversionValues(conversions) {
 
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.body;
-  const icons = document.querySelectorAll(".icon");
+  const modeToggleBtn = document.querySelector(".mode-toggler");
+  const icons = modeToggleBtn.querySelectorAll("ion-icon");
   const isDarkMode = localStorage.getItem("theme") === "dark";
 
   body.classList.toggle("dark-mode", isDarkMode);
   icons[0].classList.toggle("hidden", isDarkMode);
   icons[1].classList.toggle("hidden", !isDarkMode);
 
-  icons.forEach((icon) => {
-    icon.addEventListener("click", () => {
-      const darkModeEnabled = body.classList.toggle("dark-mode");
-      localStorage.setItem("theme", darkModeEnabled ? "dark" : "light");
+  modeToggleBtn.setAttribute(
+    "aria-label",
+    isDarkMode ? "Enable light mode" : "Enable dark mode"
+  );
+  modeToggleBtn.setAttribute("aria-pressed", isDarkMode);
 
-      icons.forEach((i) => i.classList.toggle("hidden"));
-    });
+  modeToggleBtn.addEventListener("click", () => {
+    const darkModeEnabled = body.classList.toggle("dark-mode");
+    localStorage.setItem("theme", darkModeEnabled ? "dark" : "light");
+
+    icons.forEach((icon) => icon.classList.toggle("hidden"));
+
+    modeToggleBtn.setAttribute(
+      "aria-label",
+      darkModeEnabled ? "Enable light mode" : "Enable dark mode"
+    );
+
+    modeToggleBtn.setAttribute("aria-pressed", darkModeEnabled);
   });
 });
+
+function showErrorMessage(message) {
+  errorMessage.textContent = message;
+  errorMessage.classList.add("show");
+
+  setTimeout(() => {
+    errorMessage.classList.remove("show");
+  }, 2500);
+}
